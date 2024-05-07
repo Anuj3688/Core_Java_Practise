e03455234eef7a93dd9c31218c4e6646ea70986c
Anuj Tiwari <anuj.10tiwari2001@gmail.com>
2024-05-07 21:51:18 +0530

    First Commit Jai Shree Ram

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Shortcuts for Development
+1. sout+tab `For System.out.println()`
+2. ctrl + / `For comment & uncomment`
+3. ctrl + shift +/ `For multiline comment and uncomment`
+4. ctrl + Y `For Deleting the lines`
+5. ctrl+alt+l `For Formatting the file`
+6. ctrl+d `For Copying the same line of code`
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num = 2342;
+        System.out.println(num);
+        printDetails();
+    }
+    public static void printDetails(){
+        /*
+        The Work of this function is to print required details.
+        */
+        System.out.println("Here we are starting out java development");
+    }
+
+}
